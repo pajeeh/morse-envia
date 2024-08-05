a93cdb269a19a7c97fc0530f74cbbf7db26d618c
Luis Santos <48485199+pajeeh@users.noreply.github.com>
2024-08-05 16:58:56 -0300

    Adicionado lógica para pin P2 enviar novo desenho

--- main.ts
+++ main.ts
@@ -1,10 +1,22 @@
+input.onPinPressed(TouchPin.P2, function () {
+    radio.sendNumber(1)
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # . # . #
+        # . . . #
+        # # # # #
+        `)
+    basic.pause(100)
+    basic.clearScreen()
+})
 input.onPinPressed(TouchPin.P1, function () {
     radio.sendNumber(0)
     basic.showLeds(`
         . . . . .
-        . . # . .
         . # # # .
-        . . # . .
+        . # . # .
+        . # # # .
         . . . . .
         `)
     basic.pause(100)
